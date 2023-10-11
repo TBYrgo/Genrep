d8dca69f3d9499c0c797d7fb58c523cb3d3fa52d
Tobias Blom <tobblo0507@skola.goteborg.se>
2023-10-11 14:40:09 +0200

    first commit

---
+++ genrep.py
@@ -0,0 +1,13 @@
+
+uddatal = 0
+jamnatal = 0
+
+print("Beräknar differensen av jämna/udda tal ...")
+
+for i in range(2,2001,2):
+    jamnatal += i
+
+for j in range(1,2000,2):
+        uddatal += j
+
+print(f'Differensen = {jamnatal-uddatal}')
